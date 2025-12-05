96984cf848826c490f35157d61dc5725b052415c
ElmarAliyev-7 <liyevelmar02@gmail.com>
2025-12-05 19:03:13 +0400

    fixed

--- app/DataTransferObjects/Payment/Order/CreateOrderResponseDto.php
+++ app/DataTransferObjects/Payment/Order/CreateOrderResponseDto.php
@@ -10,14 +10,14 @@ class CreateOrderResponseDto extends Dto
      * @param int $httpCode
      * @param CreateOrderDto|null $order
      * @param string|null $curlError
-     * @param string|null $curlErrno
+     * @param int|null $curlErrno
      * @param string|null $formUrl
      */
     public function __construct(
         public int $httpCode,
         public ?CreateOrderDto $order = null,
         public ?string $curlError = null,
-        public ?string $curlErrno = null,
+        public ?int $curlErrno = null,
         public ?string $formUrl = null,
     )
     {
