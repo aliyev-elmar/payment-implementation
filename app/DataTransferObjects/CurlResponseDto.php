77346e7a2084df8cbc6dfb33a80fbbf4c4b80214
ElmarAliyev-7 <liyevelmar02@gmail.com>
2025-12-05 19:16:19 +0400

    fixed

--- app/DataTransferObjects/CurlResponseDto.php
+++ app/DataTransferObjects/CurlResponseDto.php
@@ -8,13 +8,13 @@ class CurlResponseDto extends Dto
      * @param object|null $response
      * @param int $httpCode
      * @param string $curlError
-     * @param string $curlErrno
+     * @param int $curlErrno
      */
     public function __construct(
         public ?object $response,
         public int $httpCode,
         public string $curlError,
-        public string $curlErrno,
+        public int $curlErrno,
     )
     {
     }
